4d558c82868abe5f50d96c7f07ec7023df484156
Diogo Cabral <65073231+DCabral11@users.noreply.github.com>
2026-02-14 22:00:07 +0000

    Update Firebase configuration settings
    Updated Firebase configuration with new project details.

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 window.FIREBASE_CONFIG = {
-    apiKey: 'AIzaSyB2ylIhEVe2dblr6I39UoCgRJjlFBru-tc',
-    authDomain: 'cartao-jogo-digital-bp-2026.firebaseapp.com',
-    databaseURL: 'https://cartao-jogo-digital-bp-2026-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'cartao-jogo-digital-bp-2026',
-    storageBucket: "cartao-jogo-digital-bp-2026.firebasestorage.app",
-    messagingSenderId: "359987921842",
-    appId: "1:359987921842:web:4f15943185f8c1ea7a1aab",
-    measurementId: "G-HKNTSKGL53"
+    apiKey: 'AIzaSyBUybzFwEd14t1j1GxkwkT_oTqLZgU3g0M',
+    authDomain: 'cjogo-bp2026.firebaseapp.com',
+    databaseURL: 'https://cjogo-bp2026-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: 'cjogo-bp2026',
+    storageBucket: "cjogo-bp2026.firebasestorage.app",
+    messagingSenderId: "148313957869",
+    appId: "1:148313957869:web:123c6b24f386b88ab8b3e4",
+    measurementId: "G-SSEFJ29WNR"
 }
