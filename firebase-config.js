adb453862882d5695f3b7d78a025eea47566af63
Diogo Cabral <65073231+DCabral11@users.noreply.github.com>
2026-02-16 20:54:10 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 window.FIREBASE_CONFIG = {
-    apiKey: 'AIzaSyBUybzFwEd14t1j1GxkwkT_oTqLZgU3g0M',
-    authDomain: 'cjogo-bp2026.firebaseapp.com',
-    databaseURL: 'https://cjogo-bp2026-default-rtdb.europe-west1.firebasedatabase.app/',
-    projectId: 'cjogo-bp2026',
-    storageBucket: "cjogo-bp2026.firebasestorage.app",
-    messagingSenderId: "148313957869",
-    appId: "1:148313957869:web:123c6b24f386b88ab8b3e4",
-    measurementId: "G-SSEFJ29WNR"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
