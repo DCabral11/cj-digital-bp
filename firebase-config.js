5f867578941a64af35476c36772a172388d50b17
Diogo Cabral <65073231+DCabral11@users.noreply.github.com>
2026-02-16 20:46:31 +0000

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyBUybzFwEd14t1j1GxkwkT_oTqLZgU3g0M',
+    authDomain: 'cjogo-bp2026.firebaseapp.com',
+    databaseURL: 'https://cjogo-bp2026-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: 'cjogo-bp2026',
+    storageBucket: "cjogo-bp2026.firebasestorage.app",
+    messagingSenderId: "148313957869",
+    appId: "1:148313957869:web:123c6b24f386b88ab8b3e4",
+    measurementId: "G-SSEFJ29WNR"
+};
