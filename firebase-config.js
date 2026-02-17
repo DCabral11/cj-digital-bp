9323202b9746fffa23c8e89e19eeb0dd7cc99c5d
Diogo Cabral <65073231+DCabral11@users.noreply.github.com>
2026-02-17 00:28:49 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,5 @@
 window.FIREBASE_CONFIG = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: AIzaSyBUybzFwEd14t1j1GxkwkT_oTqLZgU3g0M,
+  authDomain: cjogo-bp2026.firebaseapp.com,
+  projectId: cjogo-bp2026,
 };
